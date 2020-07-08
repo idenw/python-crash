7ebe9d4391f081e84474a13029f69108f36074d7
iden <idenidenn1@gmail.com>
2020-07-08 18:41:24 +0300

    Create assignment-1.py
    assignment_1

---
+++ assignment-1.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name    :")
+txt = name.capitalize()
+if txt == "Arda" : 
+    print(f"Hello, {txt}! The password is : W@12")
+else:
+    print(f"Hello, {txt}! See you later.")
