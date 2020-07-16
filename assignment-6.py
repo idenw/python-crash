75c4381c3ead56792c1e7c01f245afa40537db50
iden <idenidenn1@gmail.com>
2020-07-16 10:04:24 +0300

    assignment-6

---
+++ assignment-6.py
@@ -0,0 +1,12 @@
+a=0
+b=[]
+c=[]
+for i in range(0,101):
+    for ii in range(1,101):
+        if i%ii+i%ii+i%ii<2:
+            b.append(i)
+for i in b:
+    if b.count(i)<3:
+        c.append(i)
+print(set(c))
+ 
